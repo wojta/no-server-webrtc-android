5af3fbd5926c9c6b58388ff0d468a5e746ab6b08
Vojtěch Sázel <vsazel@gmail.com>
2020-12-31 16:11:54 +0100

    Upgrades to new AndroidX libraries, Kotlin, etc.

--- app/src/main/kotlin/cz/sazel/android/serverlesswebrtcandroid/adapters/ConsoleAdapter.kt
+++ app/src/main/kotlin/cz/sazel/android/serverlesswebrtcandroid/adapters/ConsoleAdapter.kt
@@ -2,10 +2,7 @@ package cz.sazel.android.serverlesswebrtcandroid.adapters
 
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.content.Context
 import android.content.Context.CLIPBOARD_SERVICE
-import android.os.Build
-import android.support.v7.widget.RecyclerView
 import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
@@ -13,41 +10,39 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import androidx.recyclerview.widget.RecyclerView
 import cz.sazel.android.serverlesswebrtcandroid.R
-import org.jetbrains.anko.find
 
 /**
  * This is just to do the printing into the RecyclerView.
  */
 class ConsoleAdapter(val items: List<String>) : RecyclerView.Adapter<ConsoleAdapter.ConsoleVH>() {
 
-    @Suppress("DEPRECATION")
-    override fun onBindViewHolder(holder: ConsoleVH, position: Int) {
-        holder.tvText.text = Html.fromHtml(items[position])
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ConsoleVH {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.l_item, parent, false)
         return ConsoleVH(view)
     }
 
+
     override fun getItemCount(): Int = items.count()
 
+    override fun onBindViewHolder(holder: ConsoleVH, position: Int) {
+        holder.tvText.text = Html.fromHtml(items[position])
+    }
+
     class ConsoleVH(view: View) : RecyclerView.ViewHolder(view) {
-        var tvText: TextView = view.find(R.id.tvText)
+        var tvText: TextView = view.findViewById(R.id.tvText)
 
         init {
             tvText.setOnLongClickListener {
                 //clipboard on long touch
                 val text = tvText.text.toString()
                 val clipboard = tvText.context.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
-                clipboard.primaryClip = ClipData.newPlainText("text", text)
+                clipboard.setPrimaryClip(ClipData.newPlainText("text", text))
                 Toast.makeText(tvText.context, R.string.clipboard_copy, LENGTH_SHORT).show()
                 true
             }
         }
-
-
     }
 }
 
